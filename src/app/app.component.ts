b89d407e332cc2029a992e3fab0be32776e04b41
magz99 <magzb.99@gmail.com>
2025-08-10 21:54:41 -0400

    Upgrading Angular from 18 to 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,9 @@ import { HeaderComponent } from './components/header.component';
 import { FooterComponent } from './components/footer.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
-  template: `
+    selector: 'app-root',
+    imports: [RouterOutlet, HeaderComponent, FooterComponent],
+    template: `
     <main class="mat-app-background light-theme">
       <app-header class="w-full mx-auto max-w-screen-xl" />
       <div class="w-full mx-auto min-h-screen max-w-screen-xl p-10 my-0 ">
@@ -15,6 +14,6 @@ import { FooterComponent } from './components/footer.component';
       </div>
       <app-footer />
     </main>
-  `,
+  `
 })
 export class AppComponent {}
