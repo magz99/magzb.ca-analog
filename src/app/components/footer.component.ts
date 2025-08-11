98b61642a29a0ac7ade47b2cbfaf6751ba01b399
magz99 <magzb.99@gmail.com>
2025-08-10 23:14:18 -0400

    Upgrade to angular 20, part 1

--- src/app/components/footer.component.ts
+++ src/app/components/footer.component.ts
@@ -1,9 +1,9 @@
-import { CommonModule } from '@angular/common';
+
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
     selector: 'app-footer',
-    imports: [CommonModule],
+    imports: [],
     template: `<footer
     class="flex space-x-4 px-4 sm:px-5 lg:px-6 justify-between py-6"
   >
